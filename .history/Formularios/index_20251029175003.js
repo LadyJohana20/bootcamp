1c36a9c234418a4b5000ba308704f1a90538d4ca
¨Lady <Leidyjohanacc123@gmail.com>
2025-10-31 17:51:28 -0600

    añadir formulario

---
+++ .history/Formularios/index_20251029175003.js
@@ -0,0 +1,5 @@
+//esto es un comentario de una sola linea
+var nombre; //declaracion variable
+nombre="Juan"; //asignacion de valor
+nombre="Pedro"; //reasignacion de valor
+nombre=123; //reasignacion de valor con diferente tipo de dato
